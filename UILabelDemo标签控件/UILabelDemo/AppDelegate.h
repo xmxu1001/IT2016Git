8ea2e04b36f8789ab13c895ac4d472341b2aab1d
xmxuGitHubTest <xmxu1001@163.com>
2016-04-22 16:07:50 +0800

    Revert "Revert "123""
    This reverts commit b1783a58b482f46309e6befdde355cf1434c6537.

---
+++ UILabelDemo标签控件/UILabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UILabelDemo
+//
+//  Created by Mac on 15/11/23.
+//  Copyright © 2015年 Amy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+//@property (strong, nonatomic) UIWindow *window;属性描述声明；---strong强引用；nonatomic非原子性的 防止多线程访问同一属性的同步问题
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
