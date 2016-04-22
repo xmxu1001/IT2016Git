b4cbc0c7d9f81e9f877321a40fc59430b7879fc4
xmxuGitHubTest <xmxu1001@163.com>
2016-04-22 15:09:23 +0800

    123

---
+++ UILabelDemo标签控件/UILabelDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  UILabelDemo
+//
+//  Created by Mac on 15/11/23.
+//  Copyright © 2015年 Amy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+//UIViewController 试图控制器类
+
+
+@end
+
---
+++ UILabelDemo标签控件/UILabelDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UILabelDemo
+//
+//  Created by Mac on 15/11/23.
+//  Copyright © 2015年 Amy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
